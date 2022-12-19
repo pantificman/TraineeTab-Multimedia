760b050ddc5671e0b7645d44b212adcd02297f36
Anton <antonsoldatov1983@gmail.com>
2022-12-19 22:00:16 +0000

    JSMeet

--- script.js
+++ script.js
@@ -3,4 +3,28 @@ $("h2").css("color","green");
 $(function(){
     $("h3").css("text-align", "center");
 })
-$("h3").html("New Title 3")
+$("h3").html("New Title 3");
+
+/* alert("Hello"); */
+
+/* let a=1;
+    b="2";
+    c=true;
+function Example(a,b) {
+alert(a+b);
+}
+Example(a,b); */
+
+let answer=confirm("How old are you?");
+let answer2=prompt("Are you 18 years old?");
+
+let arr = [];
+arr[0] = prompt("question1", "someData");
+arr[1] = prompt("question2", "someData");
+arr[2] = prompt("question3", "someData");
+
+let incr = 10;
+let decr = 16;
+
+console.log(++incr);
+console.log(decr--);
